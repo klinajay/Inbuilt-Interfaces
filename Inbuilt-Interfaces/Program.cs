324016fe04f5e689f9b333d4c5b2e296f1594a85
Ajay Khandekar <ajaykhandekarklingelnberg@gmail.com>
2024-12-23 17:58:41 +0530

    Add project files.

---
+++ Inbuilt-Interfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using Inbuilt_Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        StudentRecord student1 = new StudentRecord("Ram" , 89 , 21);
+        StudentRecord student2 = new StudentRecord("shyam", 50, 21);
+
+        if(student1.CompareTo(student2) == 1 )
+        {
+            Console.WriteLine($"{student2.studentName} performed well than {student1.studentName}");
+        }
+        else if(student1.CompareTo(student2) == -1 )
+        {
+            Console.WriteLine($"{student1.studentName} performed well than {student2.studentName}");
+        }
+        else
+        {
+            Console.WriteLine($"Both {student1} and {student2} performed equally");
+        }
+    }
+}
